55c90f8f6d591a885296cd2d3104ea2b247bed35
Sam Wasden <samwasden@outlook.com>
2021-07-26 14:55:13 -0600

    created working if not slightly basic calculator

--- calc.js
+++ calc.js
@@ -14,6 +14,28 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	let solution = undefined;
+
+	if (mathSymbol === "+") {
+		solution = num1 + num2
+	} 
+	else if (mathSymbol === "-") {
+		solution = num1 - num2
+	}
+	else if (mathSymbol === "*") {
+		solution = num1 * num2
+	}
+	else if (mathSymbol === "/") {
+		solution = num1 / num2
+	}
+	else if (mathSymbol === "√") {
+		solution = Math.sqrt(num1)
+	}
+	else {
+		solution = "Please make a more intelligent input."
+	}
+
+	console.log(solution)
 
 	// This line closes the connection to the command line interface.
 	reader.close()
